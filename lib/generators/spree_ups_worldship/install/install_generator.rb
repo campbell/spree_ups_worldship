248c2181005950ac85ca81903fc77e3c26d5ce31
Pete Campbell <pete@sumirolabs.com>
2013-06-24 18:21:21 -0400

    Bugfix, removed CSS & JS asset in installation script

--- lib/generators/spree_ups_worldship/install/install_generator.rb
+++ lib/generators/spree_ups_worldship/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeUpsWorldship
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_ups_worldship\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_ups_worldship\n"
+#        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_ups_worldship\n"
+#        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_ups_worldship\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_ups_worldship\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_ups_worldship\n", :before => /\*\//, :verbose => true
+#        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_ups_worldship\n", :before => /\*\//, :verbose => true
+#        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_ups_worldship\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
