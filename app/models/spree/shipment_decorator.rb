82cfb51a76afc956781be86e61dff9f3972eb05c
Pete Campbell <pete@sumirolabs.com>
2013-07-14 13:21:55 -0400

    Remove exported shipments if the shipment is destroyed

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -2,10 +2,15 @@ module Spree
 	Shipment.class_eval do
 #		has_one :export, :class_name => 'Spree::UpsWorldship::Export'
 		after_save :update_worldship
+		before_destroy :purge_worldship
 
 		def update_worldship
 			exported_shipment = Spree::UpsWorldship::Shipment.new(self)
 			exported_shipment.sync_with_shipment
+		end
+
+		def purge_worldship
+			Spree::UpsWorldship::Export.destroy_all(shipment_number: self.number)
 		end
 
 	end
