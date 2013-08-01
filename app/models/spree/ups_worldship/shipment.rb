798d0396fb9c358ce894e23ced1d6f7688504fac
Pete Campbell <pete@sumirolabs.com>
2013-08-01 11:22:47 -0400

    Bugfix, shipments were calculating weight incorrectly

--- app/models/spree/ups_worldship/shipment.rb
+++ app/models/spree/ups_worldship/shipment.rb
@@ -67,7 +67,7 @@ class Spree::UpsWorldship::Shipment
   end
 
   def total_weight
-    weight = package_weights.reduce(:+) || shipment.line_items.collect{|i| i.quantity * i.variant.weight.to_f}.reduce(:+)
+    weight = package_weights.reduce(:+) || shipment.inventory_units.collect{|i| i.quantity * i.variant.weight.to_f}.reduce(:+)
   end
 
 
